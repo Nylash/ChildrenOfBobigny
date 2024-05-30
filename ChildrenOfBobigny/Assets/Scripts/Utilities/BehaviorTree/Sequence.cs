4eb9e65252abd827c566c518b4942817812bb9cc
Nylash <ben.peltier7@gmail.com>
2024-05-30 17:48:56 +0200

    Add missing constructors

--- ChildrenOfBobigny/Assets/Scripts/Utilities/BehaviorTree/Sequence.cs
+++ ChildrenOfBobigny/Assets/Scripts/Utilities/BehaviorTree/Sequence.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 namespace BehaviorTree
 {
     public class Sequence : Node
     {
+        public Sequence() : base() { }
+        public Sequence(List<Node> children) : base(children) { }
+
         public override NodeState Evaluate()
         {
             bool anyChildIsRunning = false;
